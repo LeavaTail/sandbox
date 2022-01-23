c57920c270d2dca367494680f86c01a1feba9ce2
LeavaTail <starbow.duster@gmail.com>
2022-01-23 14:05:40 +0900

    add

---
+++ scripts/link_diff-highlight.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -eu
+#
+# usage:
+#   check_diff-highlight.sh path
+#
+# description:
+#   create link to ${path}
+
+TARGET="diff-highlight"
+MANAGER=""
+ARRAY=""
+OUT=${1:-}
+
+function package_manager() {
+	if (type "dpkg" > /dev/null 2>&1); then
+		MANAGER="dpkg -L"
+	elif (type "rpm" > /dev/null 2>&1); then
+		MANAGER="rpm -ql"
+	fi
+}
+
+if (type ${TARGET} > /dev/null 2>&1); then
+	exit 0;
+fi
+
+package_manager
+
+if [ ! -z "$MANAGER" ]; then
+	ARRAY=`${MANAGER} git | grep "/${TARGET}$"`
+fi
+
+if [ -z ${ARRAY} ]; then
+	echo "WARN: ${TARGET} is not installed." 1>&2
+	exit 1
+fi
+
+for f in ${ARRAY}; do
+	if [ -f ${f} ] && [ -x ${f} ]; then
+		ln -s ${f} ${OUT}
+	fi
+done
