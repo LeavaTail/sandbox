5ea9cca581440d9611f1062aab3517d4d0480148
LeavaTail <starbow.duster@gmail.com>
2022-01-23 15:58:33 +0900

    12

--- scripts/link_diff-highlight.sh
+++ scripts/link_diff-highlight.sh
@@ -5,41 +5,45 @@
 #
 # description:
 #   create link to ${path}
-
 TARGET="diff-highlight"
 MANAGER=""
 ARRAY=""
 OUT=${1:-}
+DIR=""
+FOUND=false
 
-function package_manager() {
-	if (type "dpkg" > /dev/null 2>&1); then
-		MANAGER="dpkg -L"
-	elif (type "rpm" > /dev/null 2>&1); then
-		MANAGER="rpm -ql"
-	else
-		echo "None"
-	fi
-}
-
-if (type ${TARGET} > /dev/null 2>&1); then
-	exit 0;
-fi
-
-package_manager
-
-if [ ! -z "$MANAGER" ]; then
-	echo `${MANAGER} git`
-	ARRAY=($(${MANAGER} git | grep "/${TARGET}$"))
-	echo ${ARRAY}
-fi
-
-if [ -z ${ARRAY} ]; then
-	echo "WARN: ${TARGET} is not installed." 1>&2
+# diff-highlight needs git
+if !(type git > /dev/null 2>&1); then
+	echo "WARN: git is not installed." 1>&2
 	exit 1
 fi
 
-for f in ${ARRAY[@]}; do
-		ln -s ${f} ${OUT}
+# diff-higtlight location is searched by hpackage manager
+if (type "dpkg" > /dev/null 2>&1); then
+	MANAGER="dpkg -L"
+elif (type "rpm" > /dev/null 2>&1); then
+	MANAGER="rpm -ql"
+else
+	echo "WARN: package manager can't detect." 1>&2
+	exit 1
+fi
+
+TARGET_LIST=($(${MANAGER} git | grep "/${TARGET}$"))
+
+for f in ${TARGET_LIST[@]}; do
 	if [ -f ${f} ]; then
+#		ln -s ${f} ${OUT}
+		echo ${f}
+		FOUND=true
+	elif [ -d ${f} ]; then
+		DIR=${f}
+	else 
+		:
 	fi
 done
+
+if "${FOUND}"; then
+	echo "Create link from '${f}' to '${OUT}'"
+else
+	make -C ${DIR}
+fi
