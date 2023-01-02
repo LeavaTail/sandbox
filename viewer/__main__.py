f48d87fa720913019e26f59028eec38f638f82cf
LeavaTail <starbow.duster@gmail.com>
2023-01-02 22:28:16 +0900

    xfstests-result-viewer CI test
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

---
+++ viewer/__main__.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""organize the xfstests result
+
+This modules organize and format the xfstests results. This use timestamp and
+logfile as well as result directory. As a result, return the lists that is
+formatted by JSON.
+
+Example:
+
+    $ python3 __main__.py /xfstests/results
+"""
+
+import os
+import sys
+import argparse
+from logging import getLogger, StreamHandler, DEBUG, CRITICAL, WARNING
+
+from .generator import read_results
+from .conv_json import ConvJsonClass
+from .conv_excel import ConvExcelClass
+
+logger = getLogger(__name__)
+
+def set_logger(opts):
+    """Set the parameter in logger.
+
+    This module set the logger based on the mode that is "Default",
+    "Verbose mode" and "Quite mode".
+    """
+    global logger
+
+    # Set the log level
+    if (opts.verbose):
+        level = DEBUG
+    elif (opts.quite):
+        level = CRITICAL
+    else:
+        level = WARNING
+
+    handler = StreamHandler()
+    handler.setLevel(level)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    return level
+
+def get_opts():
+    """Analyze the command line argument.
+
+    This module can set the parameter by command line argument.
+      -o, --output: The strategy in output
+      -q, --quite: Restrict the message
+      -v, --verbose: Output debug message
+    """
+
+    parser = argparse.ArgumentParser(
+        prog='xfstests-test-viewer',
+        description='Output json file from xfstests result')
+
+    parser.add_argument('results', help='xfstests results directory path')
+    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('--format', help='Output format type')
+    parser.add_argument('-q', '--quite', help='Quite mode', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
+    opts = parser.parse_args()
+
+    return opts
+
+def main():
+    opts = get_opts()
+    if opts.format == 'excel':
+        conv = ConvExcelClass()
+    else:
+        conv = ConvJsonClass()
+
+    level = set_logger(opts)
+    formattedlist = read_results(opts.results)
+
+    conv.dump_results(formattedlist, opts.output)
+
+if __name__ == "__main__":
+    main()
---
+++ viewer/testcase.py
@@ -0,0 +1,113 @@
+"""xfstests result information
+
+This modules stored the xfstests results information. This information use
+timestamp and logfile as well as result directory.
+"""
+
+import sys
+from logging import getLogger
+import dataclasses
+
+logger = getLogger("__main__").getChild("testcase")
+
+@dataclasses.dataclass
+class TestClass():
+    name: str
+    sec: int
+    path: str
+    remarks: str
+    """Test result generic class
+
+    The generic class to store xfstests result
+
+    Args:
+        name (str): Testcase name
+
+    Attributes:
+        name (str): Testcase name
+        sec (int): The elapsed time (second)
+        remarks (str): Additional information (e.g. Skipped cause)
+        path (str): The pathname for xfstests result
+    """
+
+    def __init__(self, name=''):
+        self.name = name
+        self.sec = 0
+        self.remarks = ''
+        self.path = ''
+
+    def update_time(self, sec):
+        self.sec = sec
+
+    def update_summary(self, pathname):
+        pass
+
+    def update_path(self, base):
+        self.path = base + '/' + self.name
+
+
+class PassedClass(TestClass):
+    """Passed result class
+
+    The class to store passed xfstests result
+
+    Args:
+        name (str): Testcase name
+
+    Attributes:
+        name (str): Testcase name
+        sec (int): The elapsed time (second)
+        remarks (str): Additional information (e.g. Skipped cause)
+        path (str): The pathname for xfstests result
+    """
+
+
+    def __init__(self, name=''):
+        super().__init__(name)
+
+
+class SkippedClass(TestClass):
+    """Skipped result class
+
+    The class to store skipped xfstests result
+
+    Args:
+        name (str): Testcase name
+
+    Attributes:
+        name (str): Testcase name
+        sec (int): The elapsed time (second)
+        remarks (str): Additional information (e.g. Skipped cause)
+        path (str): The pathname for xfstests result
+    """
+
+    def __init__(self, name=''):
+        super().__init__(name)
+
+    def update_summary(self, pathname):
+        try:
+            with open(pathname + self.name + '.' + 'notrun', 'r') as f:
+                contents = f.read()
+        except OSError:
+            logger.error("Could not open file:" + pathname + self.name + '.' + 'notrun')
+            sys.exit()
+        self.remarks = contents
+
+
+class FailedClass(TestClass):
+    """Failed result class
+
+    The class to store failed xfstests result
+
+    Args:
+        name (str): Testcase name
+
+    Attributes:
+        name (str): Testcase name
+        sec (int): The elapsed time (second)
+        remarks (str): Additional information (e.g. Skipped cause)
+        path (str): The pathname for xfstests result
+    """
+
+    def __init__(self, name=''):
+        super().__init__(name)
